6bb8916ad351f542a59f368eb28732f75b1b3c83
Ikari <usafsow@gmail.com>
2026-03-09 19:59:17 -0700

    update

--- build.bat
+++ build.bat
@@ -1,4 +1,7 @@
 @echo off
+echo Pulling latest updates...
+git pull
+echo.
 echo Building Void Player...
 C:\Users\Ikari\AppData\Local\Programs\Python\Python312\Scripts\pyinstaller.exe --onefile --noconsole --name void_player void_player.py
 echo.
